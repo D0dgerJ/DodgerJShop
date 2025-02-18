67ec7edb223f037d03e70b51cee3c683dfec0f13
D0dgerJ <d0dgerj89@gmail.com>
2025-02-18 20:50:01 +0200

    api

--- v2/client/src/api.js
+++ v2/client/src/api.js
@@ -1,3 +1,6 @@
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_URL_LOCAL
+    : process.env.REACT_APP_API_URL_PROD;
 
 export default API_BASE;
