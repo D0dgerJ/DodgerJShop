21cfad5f7cf7fa2d4720bd7210ca1798aa9cb97b
D0dgerJ <d0dgerj89@gmail.com>
2025-02-24 12:41:10 +0200

    network fix

--- v2/client/src/api.js
+++ v2/client/src/api.js
@@ -1,6 +1,8 @@
-const API_BASE =
+/*const API_BASE =
   process.env.NODE_ENV === "development"
     ? process.env.REACT_APP_API_URL_LOCAL
     : process.env.REACT_APP_API_URL_PROD;
+*/
 
+const API_BASE = process.env.REACT_APP_API_URL_PROD || "http://79.132.3.109:5000";
 export default API_BASE;
